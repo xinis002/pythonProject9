d35a0e46bd4e9e1c4bdda866ebdc45d551c368f6
Simon Inikhov <semikinihov@gmail.com>
2024-05-06 20:40:05 +0300

    add

---
+++ catolog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from catolog.apps import CatologConfig
+from catolog.views import home, contacts
+
+app_name = CatologConfig.name
+
+
+urlpatterns = [
+    path('', home, name='home' ),
+    path('contacs', contacts, name='contacts')
+]
